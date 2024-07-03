d75d12951e788e860e6afe6935e90565226c8c49
Gokul Urs P <96222573+srulukog@users.noreply.github.com>
2024-07-03 15:16:20 +0530

    Update Program_2.c

--- Program_2.c
+++ Program_2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
 
+
+int main() {
+    char dataType[20];
+    
+    printf("Enter the data type (Integer, Long, Float, Double, Character): ");
+    scanf("%s", dataType);
+    
+    if (strcmp(dataType, "Integer") == 0) {
+        printf("Size of Integer: 4 bytes\n");
+    } else if (strcmp(dataType, "Long") == 0) {
+        printf("Size of Long: 8 bytes\n");
+    } else if (strcmp(dataType, "Float") == 0) {
+        printf("Size of Float: 4 bytes\n");
+    } else if (strcmp(dataType, "Double") == 0) {
+        printf("Size of Double: 8 bytes\n");
+    } else if (strcmp(dataType, "Character") == 0) {
+        printf("Size of Character: 1 byte\n");
+    } else {
+        printf("Unknown data type\n");
+    }
+    
+    return 0;
+}
