cf0a741e10b8eae01b31d03f373720a395a81ff4
Gokul Urs P <96222573+srulukog@users.noreply.github.com>
2024-07-03 15:25:09 +0530

    Update Program_1.c

--- Program_1.c
+++ Program_1.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int check_character(char ch) {
+if (ch >= 'A' && ch <= 'Z') {
+return 1;
+} else if (ch >= 'a' && ch <= 'z') {
+return 0;
+} else {
+return -1;
+}
+}
+int main() {
+char input_char;
+printf("Enter a character: ");
+scanf("%c", &input_char);
+int result = check_character(input_char);
+if (result == 1) {
+printf("Output: 1\n");
+} else if (result == 0) {
+printf("Output: 0\n");
+} else {
+printf("Output: -1\n");
+}
+return 0;
